e13f286386b3cb3d44f390a7eb1b076b9a01174f
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 20:04:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,29 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
   get '/hello' do
     "Hello World!"
   end
 
-  # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
 
-#   get '/goodbye/:name, a dynamic route that returns "Goodbye, (person's name).", a string.
-#   For example, navigating to localhost:9393/goodbye/jerome should display Goodbye, jerome.
-get "/goodbye/:name" do
-  @user_name = params[:name]
-  "Goodbye, #{@user_name}."
-end
-
-get "/multiply/:num1/:num2" do
-  @sum = params[:num1].to_i*params[:num2].to_i
-  "#{@sum}"
-end
+  get "/multiply/:num1/:num2" do
+    @sum = params[:num1].to_i*params[:num2].to_i
+    "#{@sum}"
+  end
 
 end
